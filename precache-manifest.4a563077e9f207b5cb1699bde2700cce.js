567f778a17dd2783f07983106e7e2d44bbd9805d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-02 00:07:16 +0000

    Deploy raftx24/vue-realworld-example-app to github.com/raftx24/vue-realworld-example-app.git:gh-pages

---
+++ precache-manifest.4a563077e9f207b5cb1699bde2700cce.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "aadab5511358ac12ffa0",
+    "url": "/js/app.469664a7.js"
+  },
+  {
+    "revision": "72d74e229000e49c0d85",
+    "url": "/js/chunk-2d0b3289.fe9dd4f7.js"
+  },
+  {
+    "revision": "0449da1f3ce1a2bf384a",
+    "url": "/js/chunk-2d0bac97.74e3c28d.js"
+  },
+  {
+    "revision": "6f5468c9d5969283ad3c",
+    "url": "/js/chunk-2d0bd246.b354ca7f.js"
+  },
+  {
+    "revision": "9f44f6920e57dc31cf73",
+    "url": "/js/chunk-2d0cedd0.ea949ae4.js"
+  },
+  {
+    "revision": "8b1c28be2b8347594da5",
+    "url": "/js/chunk-2d0d6d35.bda70b64.js"
+  },
+  {
+    "revision": "c01d6eb02662657433ba",
+    "url": "/js/chunk-2d0f1193.12c44839.js"
+  },
+  {
+    "revision": "5f492d308cef87b93826",
+    "url": "/js/chunk-2d207fb4.245dc458.js"
+  },
+  {
+    "revision": "bcf96b48efd3b36a9e7f",
+    "url": "/js/chunk-2d2086b7.2153bf02.js"
+  },
+  {
+    "revision": "d87fe9b8fa0ccdbed0f7",
+    "url": "/js/chunk-2d217357.a9fde2cd.js"
+  },
+  {
+    "revision": "29da71f49ce5a8926456",
+    "url": "/js/chunk-2d21eac7.57b2e8f8.js"
+  },
+  {
+    "revision": "c66a7a8febe4985a0acf",
+    "url": "/js/chunk-52fabea2.5a47b69d.js"
+  },
+  {
+    "revision": "38f03d8f62a76cdfd6af",
+    "url": "/js/chunk-704fe663.30e88abf.js"
+  },
+  {
+    "revision": "36b8eda030192301b4c2",
+    "url": "/js/chunk-8ab06c80.37a5b566.js"
+  },
+  {
+    "revision": "3788885c3a591a1fc58d",
+    "url": "/js/chunk-fee37f4e.4d146c30.js"
+  },
+  {
+    "revision": "94214cb7224dae9b4f94",
+    "url": "/js/chunk-vendors.dcd10e99.js"
+  },
+  {
+    "revision": "119df8bce7d673e6d383f7705934022b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
